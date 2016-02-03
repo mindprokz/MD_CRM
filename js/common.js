24dda7c7d498ff89eeb38ae74dc01a04748bab48
Merrick <krhooliga1@gmail.com>
2016-02-03 19:06:57 +0600

    last commit

--- js/common.js
+++ js/common.js
@@ -18,11 +18,12 @@ $(document).ready(function() {
 	//});
 	//Аякс отправка форм
 	//Документация: http://api.jquery.com/jquery.ajax/
-	$("#application").submit(function() {
+	$("#application").submit(function(event) {
+		event.preventDefault();
 		var data = {
-			name : document.querySelector('input[name="name"]').value,
-			email : document.querySelector('input[name="email"]').value,
-			telephone : document.querySelector('input[name="telephone"]').value,
+			name : document.querySelector('#application input[name="name"]').value,
+			email : document.querySelector('#application input[name="email"]').value,
+			telephone : document.querySelector('#application input[name="telephone"]').value,
 		}
 		$.ajax({
 			type: "POST",
@@ -31,9 +32,17 @@ $(document).ready(function() {
 		}).done(function( value ) {
 			$('#mail')[0].innerHTML = value;
 			$('#mail').removeClass('not_visible_mail');
+			$('#mail')[0].setAttribute('style', 'opacity: 1;');
 			setTimeout(function() {
-				$("#form").trigger("reset");
+				$("#application").trigger("reset");
 			}, 1000);
+			setTimeout(function() {
+				$('#mail')[0].setAttribute('style', 'opacity: 0;');
+				setTimeout(function() {
+					$('#mail').addClass('not_visible_mail');
+				}, 500);
+			}, 5000);
+
 		});
 		return false;
 	});
@@ -50,4 +59,18 @@ $(document).ready(function() {
 
 	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
 
+	document.querySelector('#get_form').onclick = function(event) {
+		event.preventDefault();
+		document.querySelector('#form').classList.remove('not_active_form');
+		document.querySelector('#form').classList.add('active_form');
+	};
+	document.querySelector('.close__form').onmousemove = function() {
+		document.querySelector('#form').setAttribute('style', '-webkit-filter:opacity(0.5)');
+	};
+	document.querySelector('.close__form').onmouseout = function() {
+		document.querySelector('#form').removeAttribute('style');
+	}
+	document.querySelector('.close__form').onclick = function() {
+		document.querySelector('#form').setAttribute('class', 'not_active_form');
+	}
 });
